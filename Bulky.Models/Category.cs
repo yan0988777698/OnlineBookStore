0e3ca91df1222cff9053500dc470a7f79521c5bf
彥霖 陳 <yan0988777698@gmail.com>
2024-05-20 23:58:17 +0800

    1. 重新改寫架構 N-Tier Architecture

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
         [Key]
         public int Id { get; set; }
         [Required]
-        [MaxLength(100,ErrorMessage = "字數上限為100")]
+        [MaxLength(100, ErrorMessage = "字數上限為100")]
         public string Name { get; set; }
-        [Range(1, 100,ErrorMessage ="請輸入1-100之間的數字")]
+        [Range(1, 100, ErrorMessage = "請輸入1-100之間的數字")]
         public int DisplayOrder { get; set; }
 
     }
