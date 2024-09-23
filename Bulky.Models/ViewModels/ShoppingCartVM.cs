08fd8b5767b816ca0537da4422665cc4b0ba69af
彥霖 陳 <yan0988777698@gmail.com>
2024-09-23 21:46:48 +0800

    Debug

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Bulky.Models.ViewModels
 {
     public class ShoppingCartVM
     {
+        [ValidateNever]
         public IEnumerable<ShoppingCart> ShoppingCartList {  get; set; }
         public OrderHeader OrderHeader { get; set; }
     }
